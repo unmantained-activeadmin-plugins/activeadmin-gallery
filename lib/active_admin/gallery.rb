be4d889b537ff7ae4a36e4a0d39fe935ac57852c
Stefano Verna <stefano.verna@welaika.com>
2013-04-30 10:43:58 +0200

    Fix

--- lib/active_admin/gallery.rb
+++ lib/active_admin/gallery.rb
@@ -1,7 +1,17 @@
-require 'active_admin/gallery/engine'
-require 'active_admin/gallery/active_record_extension'
-require 'active_admin/gallery/form_builder_extension'
+begin
+  require 'active_admin/gallery/engine'
+rescue NameError => e
+end
 
-ActiveRecord::Base.send :extend, ActiveAdmin::Gallery::ActiveRecordExtension
-ActiveAdmin::FormBuilder.send :include, ActiveAdmin::Gallery::FormBuilderExtension
+begin
+  require 'active_admin/gallery/active_record_extension'
+  ActiveRecord::Base.send :extend, ActiveAdmin::Gallery::ActiveRecordExtension
+rescue NameError => e
+end
+
+begin
+  require 'active_admin/gallery/form_builder_extension'
+  ActiveAdmin::FormBuilder.send :include, ActiveAdmin::Gallery::FormBuilderExtension
+rescue NameError => e
+end
 
