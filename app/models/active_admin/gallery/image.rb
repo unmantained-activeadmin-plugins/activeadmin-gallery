2fcd7abab3f4b6c6d6c60f3bc3c47de04ea0991f
nebirhos <me@nebirhos.com>
2014-08-01 13:02:29 +0200

    Workaround to avoid performance issues on large galleries update

--- app/models/active_admin/gallery/image.rb
+++ app/models/active_admin/gallery/image.rb
@@ -5,7 +5,7 @@ module ActiveAdmin::Gallery
 
     image_accessor :image
     validates :image, presence: true, unless: :remove_image
-    validates_size_of :image, :maximum => 3.megabyte
+    validates_size_of :image, :maximum => 3.megabyte, on: :create
     validates_property :format, of: :image, in: [:jpeg, :jpg, :png, :tif, :tiff, :gif], case_sensitive: false
 
     default_scope { order(:position) if column_names.include?('position') }
